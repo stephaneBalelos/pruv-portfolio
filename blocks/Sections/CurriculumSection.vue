d4a8560ed9c6de3196b359965e963a2f9f21ece5
stephaneBalelos <stephanedondyas2@gmail.com>
2025-01-23 23:56:05 +0100

    Curriculum

---
+++ blocks/Sections/CurriculumSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <CurriculumSection :title="props.title ?? ''">
+    <template #html>
+      <PruviousHTML :html="props.textContent" />
+    </template>
+  </CurriculumSection>
+</template>
+
+<script setup lang="ts">
+import CurriculumSection from "~/components/Sections/CurriculumSection.vue";
+import { defineBlock, editorField, textField } from "~/.pruvious";
+
+defineBlock({
+    label: "Curriculum Section",
+    description: "Curriculum section block",
+    icon: "Wallpaper",
+});
+
+const props = defineProps({
+    title: textField({ label: "Title", default: "Title", required: true }),
+    textContent: editorField({
+        label: "Text Content",
+        placeholder: "Enter your text here",
+        blockFormats: [
+        { className: "p-6 border rounded", label: "Boxed", tags: ["div"] },
+        ],
+        inlineFormats: [{ className: "text-red-500", label: "Red" }],
+        toolbar: [
+        "bold",
+        "italic",
+        "heading2",
+        "heading3",
+        "paragraph",
+        "link",
+        "blockFormats",
+        "inlineFormats",
+        "clear",
+        ],
+        required: true,
+    }),
+});
+</script>
+
+<style scoped></style>
---
+++ components/Sections/CurriculumSection.vue
@@ -0,0 +1,24 @@
+<template>
+    <UContainer>
+        <div class="relative flex items-center justify-between gap-24">
+            <div class="flex flex-col flex-1">
+                <h2 class="text-4xl font-bold text-gray-900 dark:text-gray-100">{{ props.title }}</h2>
+                <slot name="html" />
+            </div>
+        </div>
+    </UContainer>
+</template>
+
+<script setup lang="ts">
+
+type Props = {
+    title: string
+    image?: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<style scoped>
+
+</style>
