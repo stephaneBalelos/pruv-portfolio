c58c51bbcd5ca982118ca3b35406692cb4251d12
stephaneBalelos <stephanedondyas2@gmail.com>
2025-01-28 17:53:17 +0100

    Projects Animations

--- components/Sections/ProjectsListItem.vue
+++ components/Sections/ProjectsListItem.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="relative flex items-center justify-center h-screen w-full">
+  <div ref="section" class="relative flex items-center justify-center h-screen w-full">
     <UContainer class="h-full">
       <div class="relative w-full h-full">
         <div class="absolute w-9/12 h-1/2 lg:h-full top-0 right-0">
           <img
+          ref="image"
             src="https://picsum.photos/1920/1280"
             alt="Project Image"
             class="w-full h-full object-cover m-0"
           >
         </div>
         <div
+        ref="projectCard"
           class="absolute w-full lg:w-1/2 bottom-8 lg:bottom-24 left-0 flex flex-col bg-white dark:bg-gray-800 p-8 shadow-lg"
         >
           <div class="text-sm text-gray-900 dark:text-gray-200">Project</div>
           <div class="text-4xl font-bold text-gray-900 dark:text-gray-200">
-            {{ props.title }}
+            <TextSchuffle :interval="100">{{ props.title }}</TextSchuffle>
           </div>
           <div class="text-sm text-gray-900 dark:text-gray-200 mt-4">
             <PruviousHTML :html="props.description" />
@@ -49,6 +51,10 @@
 
 <script setup lang="ts">
 import { PruviousHTML } from "#components";
+import TextSchuffle from "../ui/TextSchuffle.vue";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 type Props = {
   title: string;
@@ -60,6 +66,34 @@ type Props = {
 };
 
 const props = defineProps<Props>();
+
+const section = ref<HTMLElement | null>(null);
+const image = ref<HTMLElement | null>(null);
+const projectCard = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+    if (projectCard.value && section.value && image.value) {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+            trigger: section.value,
+            start: "top top",
+            end: "center top",
+            scrub: 3,
+            toggleActions: "play pause resume reset",
+            },
+        });
+    
+        tl.to(projectCard.value, {
+            y: -500,
+            duration: 1,
+        });
+
+        tl.to(image.value, {
+            opacity: .2,
+            duration: 1,
+        }, "-=1");
+    }
+})
 
 </script>
 
